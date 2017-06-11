d34d765d9f1f348d23696bb1d4de4e2b324d0cba
Dominic English <dominic.b.english@gmail.com>
2017-06-11 18:20:59 +1000

    Added <About> page

---
+++ pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import AppFrame from '../../components/AppFrame';
+import ContentHeading from '../../components/ContentHeading';
+import Heading from '../../components/Heading';
+import BulletList from '../../components/BulletList';
+import Button from '../../components/Button';
+import Paragraph from '../../components/Paragraph';
+
+export default ({}) => (
+  <AppFrame>
+    <ContentHeading title="About Us" subtitle="Find out more about our history below"/>
+
+    <Paragraph>{`Since moving to Cairns in 2003 , Kevin English established his first Massage Centre. Initially with 5 other treatment rooms and therapists, quickly relinquished role of bringing together therapists to concentrate exclusively on the therapies. To retain the name, and its suggestion of multiple therapies available , he began some intensive further education to extend the range of therapies. A typical year still includes some 500 hours of courses.`}</Paragraph>
+
+    <Paragraph>{`Before entering the world of Natural Therapies, Kevin enjoyed a successful career as a dancer/ mime/ actor/ singer. Performing with the world renowned Lindsay Kemp Company between 1975 and 1989. Moving from Sydney to a base in Barcelona and the hectic whirl of touring the world and performing in some of Europe's finest theatres. When enough was enough, it seemed a logical progression to work in Natural Therapies, for that is what sustained such an arduous lifestyle for so many years.`}</Paragraph>
+
+    <Paragraph>{`A return to the normal world , working and studying, attained Diploma of Advanced Remedial Massage, and Diploma of Medicinal Aromatherapy in Cairns from the Northern School of Traditional Therapy. Continuing education ,and a member of Australian Traditional Medicine Society -ATMS since 1998. Extended into studying the Upledger Curriculum in Craniosacral Therapy, and as a Practitioner this year Teacher Assisted CST1 in Sydney and CST2 in Buenos Aires (in Spanish ).`}</Paragraph>
+
+    <Paragraph>{`Moving off land and into WATER THERAPIES,qualifies as a Practitioner and Introductory teacher for AQUATHERICS® in 2009 and Watsu® ,in 2008. And has adapted Craniosacral Therapy for water sessions.`}</Paragraph>
+
+    <Paragraph>{`In 2008 a custom built expanded clinic ; Aguadulgent Massage Centre was established in Whitfield, a short drive from the centre of Cairns. With no parking problems and the suburban quiet of a private spa the new centre aligns with the holistic application of Natural therapies . Health Fund rebates are available, even some online. Credit card/ Eftpos facility.`}</Paragraph>
+
+    <Paragraph>{`As well as offering Therapies, Classes are held in the Aquadulgent Therapy Pool for the various styles of Water Therapies, with a mix of International teachers.`}</Paragraph>
+
+  </AppFrame>
+);
