54d74875ccf05c049217c78b013fffcd36547199
nirrek <hi@kerr.in>
2017-06-12 12:40:09 +1000

    Add theme file

---
+++ utils/theme.js
@@ -0,0 +1,11 @@
+export const colors = {
+  blue: '#309ddf',
+  gray: '#444444',
+  darkGray: '#222222',
+  lightGray: '#dddddd',
+  lightestGray: '#eeeeee',
+  black: '#000000',
+  white: '#ffffff',
+  red: '#FE3D55',
+  green: '#0FD1AB',
+};
