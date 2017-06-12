f7f2214497d6cd561b4a43d7e94cf89407f43e8e
nirrek <hi@kerr.in>
2017-06-12 13:26:20 +1000

    Add all pages to the exportsPathMap

--- next.config.js
+++ next.config.js
@@ -2,6 +2,28 @@ module.exports = {
   exportPathMap: function() {
     return {
       '/': { page: '/' },
+      '/therapies/aquatictherapycairns.php': {
+        page: '/therapies/aquatictherapycairns.php',
+      },
+      '/therapies/aromatherapycairns.php': {
+        page: '/therapies/aromatherapycairns.php',
+      },
+      '/therapies/craniosacraltherapycairns.php': {
+        page: '/therapies/craniosacraltherapycairns.php',
+      },
+      '/therapies/hotstonetherapycairns.php': {
+        page: '/therapies/hotstonetherapycairns.php',
+      },
+      '/therapies/remedialmassagecairns.php': {
+        page: '/therapies/remedialmassagecairns.php',
+      },
+      '/therapies/synergistictherapies.php': {
+        page: '/therapies/synergistictherapies.php',
+      },
+      '/about.php': { page: '/about.php' },
+      '/bookings.php': { page: '/bookings.php' },
+      '/contactus.php': { page: '/contactus.php' },
+      '/pricings.php': { page: '/pricings.php' },
     };
   },
 };
